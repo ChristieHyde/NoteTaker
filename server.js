02741594896e785aab87826ad2d8aa2129baeb1d
ChristieHyde <christieahyde@gmail.com>
2023-12-31 22:37:59 +1100

    Added functionality for the API routes

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const express = require('express');
 const path = require('path');
 
 // Server port and declaration
-const PORT = 3001;
+const PORT = 3002;
 const app = express();
+
+// API routing
+const api = require('./routes/index');
+app.use('/api', api);
 
 // HTML ROUTES
 app.get('/notes', (req, res) => res.send('notes'));
 
-// 
-app.get('/*', (req, res) => res.send('*'))
-
-// API ROUTES
-app.get('/api/notes', (req, res) => res.send('API get'));
-
-app.post('/api/notes', (req, res) => res.send('API post'));
+app.get('/*', (req, res) => res.send('*'));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
